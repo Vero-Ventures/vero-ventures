2166e6f64a3dc1d763b758867a4e05975a5905ad
scottchen98 <scott.chen1117@gmail.com>
2024-08-21 16:55:00 -0700

    add TechnologiesSection component

---
+++ src/components/technologies/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export function TechnologiesSection() {
+  return (
+    <section id="technology" className="border-b pt-8">
+      <div className="mx-auto mt-10 max-w-3xl">
+        <h2 className="text-center text-4xl font-bold">Technologies Used</h2>
+        <p className="my-4 text-center text-muted-foreground">
+          Our team leverages the latest technologies and frameworks, including
+          React, TypeScript, Next.JS, and more, to deliver cutting-edge software
+          solutions tailored to your needs.
+        </p>
+      </div>
+    </section>
+  );
+}
