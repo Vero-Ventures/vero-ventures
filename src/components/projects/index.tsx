84cb9fb43e76045e6eadd485be865ca0560c28da
Michael Lei <michaelleie0ca@gmail.com>
2024-08-21 12:49:06 -0700

    Finish projects section with images

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,10 +1,11 @@
 import type { ReactNode } from "react";
 import { Button } from "../ui/button";
+import Image from "next/image";
 
 export function OurWorkSection() {
   return (
     <section id="work" className="border-y">
-      <div className="mx-auto mt-10 max-w-screen-xl p-8">
+      <div className="mx-auto mt-10 p-8">
         <h2 className="text-center text-4xl font-bold">🚀 Our Work</h2>
         <p className="my-4 text-center text-muted-foreground">
           A look at some of the amazing web / mobile applications that we've
@@ -12,59 +13,32 @@ export function OurWorkSection() {
         </p>
         <div className="flex flex-col gap-20 py-20">
           <ProjectCard>
+            <ProjectImage
+              src="/projects/dynamic-needs-analysis.png"
+              alt="Dynamic Needs Analysis"
+            />
             <ProjectContent>
               <ProjectTitle>Dynamic Needs Analysis</ProjectTitle>
               <ProjectDescription>
-                <p>
-                  The comprehensive tool designed to optimize life insurance
-                  solutions for your clients.
-                </p>
-                <p className="leading-relaxed">
-                  Tailored for industry professionals, our platform offers
-                  precise coverage analysis, customizable inputs, and advanced
-                  client management features, ensuring your clients receive the
-                  ideal coverage every time.
-                </p>
+                Comprehensive tool designed to optimize life insurance solutions
+                for your clients.
               </ProjectDescription>
               <ProjectLinkButton href="https://www.dynamicneedsanalysis.com/" />
             </ProjectContent>
-            <ProjectGallery>
-              <ProjectImage
-                src="https://placehold.co/365x218"
-                alt="placeholder"
-              />
-              <ProjectImage
-                src="https://placehold.co/365x218"
-                alt="placeholder"
-              />
-            </ProjectGallery>
           </ProjectCard>
           <ProjectCard>
             <ProjectContent>
               <ProjectTitle>Clasibot</ProjectTitle>
               <ProjectDescription>
-                <p className="leading-relaxed">
-                  An AI-powered transaction classification tool designed to
-                  simplify bookkeeping
-                </p>
-                <p className="leading-relaxed">
-                  By integrating machine learning with API access to QuickBooks,
-                  Clasibot automates the categorization process, saving time for
-                  businesses.
-                </p>
+                AI-powered transaction classification tool designed to simplify
+                bookkeeping
               </ProjectDescription>
-              <ProjectLinkButton href="https://www.dynamicneedsanalysis.com/" />
+              <ProjectLinkButton href="https://www.clasibot.com/" />
             </ProjectContent>
-            <ProjectGallery>
-              <ProjectImage
-                src="https://placehold.co/365x218"
-                alt="placeholder"
-              />
-              <ProjectImage
-                src="https://placehold.co/365x218"
-                alt="placeholder"
-              />
-            </ProjectGallery>
+            <ProjectImage
+              src="/projects/dynamic-needs-analysis.png"
+              alt="Dynamic Needs Analysis"
+            />
           </ProjectCard>
         </div>
       </div>
@@ -74,22 +48,28 @@ export function OurWorkSection() {
 
 function ProjectCard({ children }: { children: ReactNode }) {
   return (
-    <article className="flex flex-col gap-6 md:flex-row">{children}</article>
+    <article className="flex flex-col justify-evenly gap-6 px-4 md:flex-row">
+      {children}
+    </article>
   );
 }
 
 function ProjectContent({ children }: { children: ReactNode }) {
-  return <div className="space-y-4">{children}</div>;
+  return <div className="mt-10 space-y-4">{children}</div>;
 }
 
 function ProjectTitle({ children }: { children: ReactNode }) {
-  return <h3 className="max-w-xs text-2xl font-bold">{children}</h3>;
+  return (
+    <h3 className="max-w-xs text-2xl font-semibold tracking-tighter">
+      {children}
+    </h3>
+  );
 }
 function ProjectDescription({ children }: { children: ReactNode }) {
   return (
-    <div className="max-w-xs space-y-4 text-sm text-muted-foreground">
+    <p className="max-w-2xl text-balance text-4xl font-bold leading-relaxed">
       {children}
-    </div>
+    </p>
   );
 }
 
@@ -101,12 +81,10 @@ function ProjectLinkButton({ href }: { href: string }) {
   );
 }
 
-function ProjectGallery({ children }: { children: ReactNode }) {
-  return <div className="flex flex-col gap-4 md:flex-row">{children}</div>;
-}
-
 function ProjectImage({ src, alt }: { src: string; alt: string }) {
   return (
-    <img src={src} alt={alt} width={365} height={218} className="rounded-lg" />
+    <div>
+      <img src={src} alt={alt} className="w-full max-w-screen-lg" />
+    </div>
   );
 }
