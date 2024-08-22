da8a28834ce0f82714efe187e5a41fff65328d9d
scottchen98 <scott.chen1117@gmail.com>
2024-08-22 12:17:12 -0700

    make separator take up viewport width between services and technolgies sections

--- src/components/services/index.tsx
+++ src/components/services/index.tsx
@@ -7,47 +7,49 @@ import type { ReactNode } from "react";
 
 export function OurServicesSection() {
   return (
-    <section
-      id="services"
-      className="mx-auto max-w-screen-2xl space-y-20 border-b pb-28 pt-8"
-    >
-      <div className="mx-auto mt-10 max-w-3xl">
-        <h2 className="text-center text-4xl font-bold">Our Services</h2>
-        <p className="my-4 text-center text-muted-foreground">
-          We offer custom software development, web and mobile application
-          creation, and technology consulting to help businesses innovate, grow,
-          and succeed in the digital landscape.
-        </p>
-      </div>
+    <section id="services" className="border-b pb-28 pt-8">
+      <div className="mx-auto max-w-screen-2xl space-y-20">
+        <div className="mx-auto mt-10 max-w-3xl">
+          <h2 className="text-center text-4xl font-bold">Our Services</h2>
+          <p className="my-4 text-center text-muted-foreground">
+            We offer custom software development, web and mobile application
+            creation, and technology consulting to help businesses innovate,
+            grow, and succeed in the digital landscape.
+          </p>
+        </div>
 
-      <div className="grid grid-cols-1 gap-7 px-8 md:grid-cols-2 lg:grid-cols-3">
-        <ServiceCard>
-          <PencilRulerIcon className="mb-3" width={36} height={36} />
-          <ServiceHeader>Custom Software Development</ServiceHeader>
-          <ServiceDescription>
-            Tailor-made software solutions designed to meet your business&apos;s
-            unique needs. We deliver scalable, high-performance software that
-            aligns with your goals and drives growth.
-          </ServiceDescription>
-        </ServiceCard>
-        <ServiceCard>
-          <SquareTerminalIcon className="mb-3" width={36} height={36} />
-          <ServiceHeader>Web and Mobile Application Development</ServiceHeader>
-          <ServiceDescription>
-            End-to-end development of responsive, user-friendly web and mobile
-            applications. We focus on delivering seamless digital experiences
-            that enhance user engagement and satisfaction.
-          </ServiceDescription>
-        </ServiceCard>
-        <ServiceCard>
-          <LightbulbIcon className="mb-3" width={36} height={36} />
-          <ServiceHeader>Technology Consulting and Strategy</ServiceHeader>
-          <ServiceDescription>
-            Expert guidance on digital transformation, technology adoption, and
-            aligning your IT strategy with business goals. We help you make
-            informed decisions that optimize efficiency and innovation.
-          </ServiceDescription>
-        </ServiceCard>
+        <div className="grid grid-cols-1 gap-7 px-8 md:grid-cols-2 lg:grid-cols-3">
+          <ServiceCard>
+            <PencilRulerIcon className="mb-3" width={36} height={36} />
+            <ServiceHeader>Custom Software Development</ServiceHeader>
+            <ServiceDescription>
+              Tailor-made software solutions designed to meet your
+              business&apos;s unique needs. We deliver scalable,
+              high-performance software that aligns with your goals and drives
+              growth.
+            </ServiceDescription>
+          </ServiceCard>
+          <ServiceCard>
+            <SquareTerminalIcon className="mb-3" width={36} height={36} />
+            <ServiceHeader>
+              Web and Mobile Application Development
+            </ServiceHeader>
+            <ServiceDescription>
+              End-to-end development of responsive, user-friendly web and mobile
+              applications. We focus on delivering seamless digital experiences
+              that enhance user engagement and satisfaction.
+            </ServiceDescription>
+          </ServiceCard>
+          <ServiceCard>
+            <LightbulbIcon className="mb-3" width={36} height={36} />
+            <ServiceHeader>Technology Consulting and Strategy</ServiceHeader>
+            <ServiceDescription>
+              Expert guidance on digital transformation, technology adoption,
+              and aligning your IT strategy with business goals. We help you
+              make informed decisions that optimize efficiency and innovation.
+            </ServiceDescription>
+          </ServiceCard>
+        </div>
       </div>
     </section>
   );
