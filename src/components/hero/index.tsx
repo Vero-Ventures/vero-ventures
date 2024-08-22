e396616a6e84c2e23f6c1a218ea753cfb2a71b3d
scottchen98 <scott.chen1117@gmail.com>
2024-08-21 22:23:01 -0700

    use hero hihglight components

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,15 +1,38 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 import type { ReactNode } from "react";
+import { HeroHighlight, Highlight } from "../ui/hero-highlight";
 
 export function HeroSection() {
   return (
-    <section className="mt-20 p-8">
-      <div className="mx-auto max-w-screen-xl">
-        <HeroTitle>Innovative Software Solutions for Your Business</HeroTitle>
-        <HeroDescription>
-          We transform your ideas into powerful, scalable software. Let's build
-          the future together.
-        </HeroDescription>
-      </div>
+    <section>
+      <HeroHighlight className="mt-10">
+        <motion.div
+          initial={{
+            opacity: 0,
+            y: 20,
+          }}
+          animate={{
+            opacity: 1,
+            y: [20, -5, 0],
+          }}
+          transition={{
+            duration: 0.5,
+            ease: [0.4, 0.0, 0.2, 1],
+          }}
+          className="mx-auto max-w-screen-xl"
+        >
+          <HeroTitle>Innovative Software Solutions for Your Business</HeroTitle>
+          <HeroDescription>
+            <Highlight>
+              We transform your ideas into powerful, scalable software. Let's
+              build the future together.
+            </Highlight>
+          </HeroDescription>
+        </motion.div>
+      </HeroHighlight>
     </section>
   );
 }
@@ -24,7 +47,7 @@ export function HeroTitle({ children }: { children: ReactNode }) {
 
 export function HeroDescription({ children }: { children: ReactNode }) {
   return (
-    <p className="font-regular relative mx-auto mb-20 max-w-3xl text-center text-base text-muted-foreground">
+    <p className="text-md relative mx-auto mb-20 max-w-3xl text-center font-semibold text-muted-foreground">
       {children}
     </p>
   );
