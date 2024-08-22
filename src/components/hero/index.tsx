27d6b5de4c07f3e92ab7c2a8f509ab57bde3d231
scottchen98 <scott.chen1117@gmail.com>
2024-08-22 11:25:42 -0700

    refactor HeroSection component and add GradualSpacing component

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -4,51 +4,51 @@ import { motion } from "framer-motion";
 
 import type { ReactNode } from "react";
 import { HeroHighlight, Highlight } from "../ui/hero-highlight";
+import { GradualSpacing } from "../ui/gradual-spacing";
 
 export function HeroSection() {
   return (
     <section>
-      <HeroHighlight className="mt-10">
-        <motion.div
-          initial={{
-            opacity: 0,
-            y: 20,
-          }}
-          animate={{
-            opacity: 1,
-            y: [20, -5, 0],
-          }}
-          transition={{
-            duration: 0.5,
-            ease: [0.4, 0.0, 0.2, 1],
-          }}
-          className="mx-auto max-w-screen-xl"
-        >
-          <HeroTitle>Innovative Software Solutions for Your Business</HeroTitle>
-          <HeroDescription>
-            <Highlight>
-              We transform your ideas into powerful, scalable software. Let's
-              build the future together.
-            </Highlight>
-          </HeroDescription>
-        </motion.div>
+      <HeroHighlight className="mx-auto mt-10 max-w-screen-xl">
+        <HeroTitle text="Innovative Software Solutions for Your Business" />
+        <HeroDescription>
+          <Highlight>
+            We transform your ideas into powerful, scalable software. Let&apos;s
+            build the future together.
+          </Highlight>
+        </HeroDescription>
       </HeroHighlight>
     </section>
   );
 }
 
-export function HeroTitle({ children }: { children: ReactNode }) {
+export function HeroTitle({ text }: { text: string }) {
   return (
-    <h1 className="mb-14 text-balance text-center text-4xl font-bold tracking-tighter md:text-7xl">
-      {children}
-    </h1>
+    <GradualSpacing
+      className="mb-8 text-balance text-center text-4xl font-bold tracking-tight md:mb-14 md:text-7xl"
+      text={text}
+    />
   );
 }
 
 export function HeroDescription({ children }: { children: ReactNode }) {
   return (
-    <p className="text-md relative mx-auto mb-20 max-w-3xl text-center font-semibold text-muted-foreground">
+    <motion.p
+      initial={{
+        opacity: 0,
+        y: 20,
+      }}
+      animate={{
+        opacity: 1,
+        y: [20, -5, 0],
+      }}
+      transition={{
+        duration: 0.5,
+        ease: [0.4, 0.0, 0.2, 1],
+      }}
+      className="text-md relative mx-auto mb-20 max-w-3xl text-center font-semibold text-muted-foreground"
+    >
       {children}
-    </p>
+    </motion.p>
   );
 }
