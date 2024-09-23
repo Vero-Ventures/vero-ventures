0530de4bc56786b58fec8db241a9e2c2219f7814
scottchen98 <scott.chen1117@gmail.com>
2024-09-23 16:00:25 -0700

    update Calendly url
    Co-authored-by: Michael Lei <michaelleie0ca@gmail.com>

--- src/components/contact-us/index.tsx
+++ src/components/contact-us/index.tsx
@@ -10,7 +10,7 @@ export function ContactUsSection() {
         <p className="mb-8 mt-2 text-lg">
           Get in touch with us for any inquiries or project discussions.
         </p>
-        <CalendlyEmbed url="https://calendly.com/yaniv-talmor-veroventures" />
+        <CalendlyEmbed url="https://calendly.com/veroventures" />
       </div>
     </section>
   );
